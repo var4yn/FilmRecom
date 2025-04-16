f0fb06df726f10d0bc071569a33d1c93358c6ea9
var4yn <2016slavyakinnikita@gmail.com>
2025-04-16 19:51:53 +0800

    init project

---
+++ src/main/java/com/github/var4yn/FilmRecom/FilmRecomApplication.java
@@ -0,0 +1,13 @@
+package com.github.var4yn.FilmRecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/var4yn/FilmRecom/FilmRecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.var4yn.FilmRecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
